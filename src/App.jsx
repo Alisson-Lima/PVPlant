9146c6dc2373f77d32239348871b25ac9a266499
Alisson Renan <renalima.rl46@gmail.com>
2022-08-19 18:32:10 -0300

    Adding main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BgImage } from "./components/BgImage/"
 import { Nav } from "./components/Nav/"
+import { Main } from "./components/Main/"
 function App() {
   return (
     <div className="App">
-      <BgImage />
       <Nav />
+      <Main />
+      <BgImage />
     </div>
   )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,31 @@
 export function Main(){
     return (
-        <main></main>
+        <main>
+            <header>
+                <div className="search-bar">
+                    <input type="text" placeholder="Search"/>
+                    <span>
+                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14 14L19 19M16 9C16 12.866 12.866 16 9 16C5.13401 16 2 12.866 2 9C2 5.13401 5.13401 2 9 2C12.866 2 16 5.13401 16 9Z" stroke="#252525" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                    </span>
+                    </div>
+            </header>
+            <article className="Title">
+                <h1>Paisagismo</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat veniam maxime repellendus ipsam asperiores dolor aspernatur obcaecati quasi facilis pariatur, odio deleniti eveniet qui eos eaque voluptatem, temporibus eligendi!</p>
+                <a href="#" className="CTA-button">See More</a>
+            </article>
+            <div className="others">
+                <article>
+                    <h3>Lorem Ipsum</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus dui, finibus non condimentum vitae.</p>
+                </article>
+                <article>
+                    <h3>Lorem Ipsum</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus dui, finibus non condimentum vitae.</p>
+                </article>
+            </div>
+        </main>
     )
 }
