10e5a2d1504169f228d7a516061904197ee1322c
Alisson Renan <renalima.rl46@gmail.com>
2022-08-20 19:02:06 -0300

    Adjusting image size

--- src/components/BgImage/index.jsx
+++ src/components/BgImage/index.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
 import ImageDesktop from "../../assets/image/desktop-image.png"
 import ImageMobile from "../../assets/image/mobile-image.png"
 import "./style.css"
 export function BgImage(){
+    const [imageWidth, setImageWidth] = useState(ImageDesktop)
 
+    function widthImageFun(){
+        let screenWidth = window.innerWidth
+        if(screenWidth <= 1000){
+            setImageWidth(ImageMobile)
+            console.log(imageWidth)
+        }else if(screenWidth > 1000){
+            setImageWidth(ImageDesktop)
+            console.log(imageWidth)
+        }
+
+    }
     return(
-        <div className="bgImage">
-            <img src={ImageMobile} alt="" />
+        <div className="bgImage" onChange={widthImageFun}>
+            <img src={imageWidth} alt="" />
         </div>
     )
 }
